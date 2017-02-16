61812d01c8cf81d77d20fa1a79329d72dc6ec735
Jinuk <Jinuk@Jinuk-Laptop>
2017-02-16 19:09:54 +0900

    준비작업 -ing

---
+++ ex01/src/main/java/org/test/persistence/BoardDAO.java
@@ -0,0 +1,14 @@
+package org.test.persistence;
+
+import java.util.List;
+
+import org.test.domain.BoardVO;
+
+public interface BoardDAO {
+	
+	public void create(BoardVO vo)throws Exception;
+	public BoardVO read(Integer bno)throws Exception;
+	public void update(BoardVO vo)throws Exception;
+	public void delete(Integer bno)throws Exception;
+	public List<BoardVO> listAll()throws Exception;
+}
