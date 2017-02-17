3749407c0faae2096678d1247fff484d3c512411
Jinuk <Jinuk@Jinuk-Laptop>
2017-02-17 17:08:51 +0900

    리스트 구현

--- ex01/src/main/java/org/test/controller/BoardController.java
+++ ex01/src/main/java/org/test/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.test.domain.BoardVO;
 import org.test.service.BoardService;
 
@@ -25,6 +26,7 @@ public class BoardController {
 		logger.info("register get ...........");
 	}
 	
+	/*
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String registPOST(BoardVO board, Model model) throws Exception {
 		logger.info("regist post ...........");
@@ -37,11 +39,27 @@ public class BoardController {
 		//return "/board/success";
 		return "redirect:/board/listAll";
 	}
+	*/
+	// RedirectAttributes의 addFlashAttribute를 이용하면 uri 뒤에붙는 부가정보를 가릴수 있다.
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registPOST(BoardVO board, RedirectAttributes rttr)
+	throws Exception {
+		
+		logger.info("regist post ...........");
+		logger.info(board.toString());
+		
+		service.regist(board);
+		
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		return "redirect:/board/listAll";
+	}
 	
 	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
 	public void listAll(Model model) throws Exception {
 		
 		logger.info("show all list...........");
+		model.addAttribute("list", service.listAll());
 		
 	}
+	
 }
